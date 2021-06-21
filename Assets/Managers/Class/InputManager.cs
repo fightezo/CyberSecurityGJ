d55b6861f0a8bc65088313e32977aaeb8c7c165f
Douglas Wong <fight320@hotmail.com>
2021-06-21 15:31:36 +0800

    test for webgl

---
+++ Assets/Managers/Class/InputManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject _hitItem;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var maxDistance = 100.0f;
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
+                Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+                _hitItem = hit.transform.gameObject;
+                _hitItem.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.white);
+                if (_hitItem != null)
+                {
+                    _hitItem.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+                    _hitItem = null;
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) //focus
+        {
+            // Camera.main.orthographic = !Camera.main.orthographic;
+        }
+    }
+}
