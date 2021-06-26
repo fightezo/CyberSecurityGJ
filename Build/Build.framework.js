fe618ce867ea75a82c193f395032ef5296f4d6b2
Douglas Wong <fight320@hotmail.com>
2021-06-26 20:18:42 +0800

    [DEPLOY] New Testing Build
    Remove unneeded files

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -1308,7 +1308,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2998064;
+STATICTOP = STATIC_BASE + 3001056;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3358,7 +3358,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2998064;
+var STATIC_BUMP = 3001056;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
